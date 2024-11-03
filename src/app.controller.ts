d4c43463b9dd18d295021fdbce2ad495ffaa239d
Victor Vitalino <victorvitalino237@gmail.com>
2024-11-03 15:36:56 -0300

    :construction: feat(DATABASE): add database module and prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { PrismaService } from "./database/database.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    return await this.prisma.client.user.findMany();
   }
 }
---
+++ src/database/database.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaService {
+  public client: PrismaClient;
+
+  constructor() {
+    this.client = new PrismaClient();
+  }
+}
